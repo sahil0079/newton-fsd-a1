eef9cbadf7cad019bbb2745cbfd3dd758c3d77ec
sahil0079 <114945558+sahil0079@users.noreply.github.com>
2023-09-29 23:23:14 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,273 @@
+// alert('Hello, world!')
+
+
+//Code Structure
+
+
+// alert('Hello, world!'); alert('Hello, world!');
+
+
+// alert('Hello, world!');
+// alert('Hello, world!');
+
+//implicit and explicit
+
+//explicit
+// alert('Hello, world!');
+// alert('Hello, world!');
+
+//implicit
+// alert('Hello, world!')
+// alert('Hello, world!')
+
+// console.log('hello world');
+
+// [10, 20, 30].forEach(alert);
+
+
+
+//variable
+
+//named storage
+
+// let message = 'hello';
+
+// message = 100;
+
+// console.log(message)
+
+// //declaration
+// let num;
+
+// //initialization
+// num = 10;
+
+
+// let num = 10;
+
+
+
+// let num = 10;
+
+// let num2;
+
+// num2 = num;
+
+// alert(num2);
+
+
+//variable naming
+
+//2 limitation
+//name must contain letters, digits , $ and _
+//first char of the variable must not be a digit
+
+// let username;
+//camelCase
+// let userName;
+// let veryBigVariableName;
+// let user1234;
+
+// let $ = 10;
+// let _ = 20;
+
+// console.log(_ + $);
+
+// let 2b;
+
+// let my-name;
+
+//casing matters
+
+// let username = '10';
+
+// console.log(USERNAME)
+
+
+//reserved keywords
+
+// let var = 10
+// let let = 10
+// let return = 200
+
+
+// var num = 100;
+
+// console.log(num)
+
+
+// var message = 'hello world';
+
+// function greet() {
+
+//     var message = 'Hi';
+//     console.log(message)
+
+
+// }
+// console.log(message)
+// greet()
+
+
+
+// message = 'hello world';
+
+// function greet() {
+
+//     message = 'Hi';
+//     console.log(message)
+
+
+// }
+// console.log(message)
+// greet()
+
+
+
+
+// function greet() {
+//     var message = 'Hi';
+// }
+// greet()
+// console.log(message)
+
+
+// function greet() {
+//     message = 'Hi';
+// }
+// greet()
+// console.log(message)
+
+
+
+
+// function x() {
+
+//     function y() {
+
+//         function z() {
+//             var message = 'hello';
+//         }
+//         z();
+//     }
+//     y();
+// }
+// x();
+
+// console.log(message)
+
+
+
+//constants
+
+//constant variable or unchanging variable
+
+const birthdayDate = '29/09/2023';
+
+//100 lines of code
+
+// birthdayDate = '10/02/2023'
+
+
+//use const for difficult to remeber values
+
+// const RED_COLOR = "#FF0012";
+
+// const FONT_SIZE = '12px';
+
+
+
+// #FF0012
+
+
+
+// #FF0012
+
+
+// #FF0012
+
+// #FF0012
+
+// #FF0012
+
+
+//DAta Types
+
+//8 Data types in javascript
+
+//Number
+
+// let n = 100;
+
+// n = 10.23456;
+
+// console.log(n)
+
+//special numeric values belong to Number type
+
+
+// console.log(typeof Infinity)
+
+// console.log(typeof -Infinity)
+
+// console.log('hello' / 2);
+
+
+// NaN => not a Number. represents computational error
+
+// console.log(NaN / 5)
+
+// console.log(NaN - NaN)
+
+// console.log(NaN ** 0)
+
+
+//BigInt
+
+// > 2 power 53 - 1 and  < -(2 power 53 - 1)
+
+// console.log(typeof 237894239479823984789324982398423949329847983247237423723489n)
+
+//String
+
+
+// let str = 'hello world';
+
+// let str2 = "this is string two";
+
+// //backtics => extended functionality
+
+// console.log(`The result of 10 + 20 is ${10 + 20}`)
+
+
+//Boolean (logical type)
+//true or false
+
+// let isAgeInputChecked = true;
+
+
+
+// let isLarger = 10 > 20;
+
+// console.log(isLarger);
+
+
+// null
+
+//separate type
+
+// let age = null;
+
+//null => nothing, empty, value unknown
+
+
+//undefined
+
+//value is not assigned
+
+// let age;
+// console.log(age)
+
+
+//Object and symbols
+// key value pairs
