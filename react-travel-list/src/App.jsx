db2d6d358dba29b73670702ae12fc10fffcf1f70
sahil0079 <114945558+sahil0079@users.noreply.github.com>
2024-01-05 23:18:20 +0530

    Add files via upload

--- react-travel-list/src/App.jsx
+++ react-travel-list/src/App.jsx
@@ -12,13 +12,39 @@ function App() {
 
     setItems((items) => [...items, item]);
 
+  };
+
+  function handleClearList() {
+
+    const confirmed = window.confirm(
+      'Are you sure you want to delete all items?'
+    );
+
+    if (confirmed) setItems([]);
+
+  };
+
+  function handleDeleteItem(id) {
+
+    setItems((items) => items.filter((item) => item.id !== id));
+  };
+
+  function handleToggleItem(id) {
+
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    )
   }
+
+
 
   return (
     <div className='app'>
       <Header />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} onClearList={handleClearList} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
       {/* <Stats/> */}
     </div>
   )
--- react-travel-list/src/components/Item.jsx
+++ react-travel-list/src/components/Item.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Item({ item }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
     return (
         <li>
-            <input type='checkbox' />
-            <span> {item.quantity} {item.description}</span>
-            <button>x</button>
+            <input type='checkbox' value={item.packed} onChange={() => onToggleItem(item.id)} />
+            <span style={item.packed ? { textDecoration: 'line-through' } : {}} > {item.quantity} {item.description}</span>
+            <button onClick={() => onDeleteItem(item.id)} >x</button>
         </li>
     )
 }
